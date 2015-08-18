db34ee6881857b9f1306d22a01e92f37bc4ce02a
Carlo Pacioni <carlo.pacioni@gmail.com>
2015-08-18 20:00:30 +0800

    fixed bug in ssmd2xlsx that would prevent the nametab from being correctly shorten

--- R/generic.helper.fun.R
+++ R/generic.helper.fun.R
@@ -25,9 +25,9 @@ iter.folders <- function(dir.path, scenario) {
 #' @import XLConnect
 #' @export
 ssmd2xlsx <- function(i, scenarios, ssmds, pvalues, wb) {
-  suppressWarnings(if(nchar(scenarios) > 25) {
+  if(nchar(scenarios[1]) > 25) {
     scenarios <- substr(scenarios, nchar(scenarios) - 24, nchar(scenarios)) 
-  })
+  }
   createSheet(wb, name=paste0("SSMD_", scenarios[i]))
   writeWorksheet(wb, ssmds[[i]], sheet=paste0("SSMD_", scenarios[i]))
   createSheet(wb, name=paste0("pval_", scenarios[i]))
