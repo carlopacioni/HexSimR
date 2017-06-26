1d6d5d25fe99751d3ee52f3f5c93b6aad71da3bd
Carlo Pacioni <carlo.pacioni@gmail.com>
2017-06-27 09:57:37 +1000

    added test make.Xpath

--- tests/testthat/test-scenarios.batch.modifier.R
+++ tests/testthat/test-scenarios.batch.modifier.R
@@ -1,6 +1,6 @@
 library(HexSimR)
 library(xml2)
-context("scenarios.batch.modifier")
+context("xml_utilities")
 
 test_that("scenarios.batch.modifier", {
   new <- system.file("extdata", "MRVC_4BaitYr_ThreeCells.xml", package="HexSimR")
@@ -18,6 +18,28 @@ test_that("scenarios.batch.modifier", {
   test_old <- read_xml(file.path(testFolder, "MRVC_4BaitYr_ThreeCells_old.xml"))
   expect_equal(test_new, test_old)
   
-  unlink(duplicate, recursive = TRUE)
+  unlink(testFolder, recursive = TRUE)
+  
+})
+
+test_that("make.Xpath", {
+  nodes_paths <- c("/scenario/population/initialSize", 
+                   "/scenario/spatialDataSeries/name",                 
+                   "/scenario/spatialDataSeries/name", 
+                   "/scenario/population/traits/accumulatedTrait/name",
+                   "/scenario/spatialDataSeries/name")
+  identifiers <- c("FALSE", "Fence_sections_2perm", "Shooting_5", 
+                   "BaitRates", "EightCells" )
+  attribs <- c(FALSE, FALSE, FALSE,  TRUE, FALSE)
+  test_norm <- list()
+  for(i in 1:5) {
+    test_norm[[i]] <- make.Xpath(nodes_paths[i], identifiers[i], attribs[i])
+  }
+  
+  expect_equal(test_norm[[2]], 
+               "/scenario/spatialDataSeries[name='Fence_sections_2perm']")
+  expect_equal(test_norm[[4]], 
+               "/scenario/population/traits/accumulatedTrait[@name='BaitRates']")
+  
   
 })
