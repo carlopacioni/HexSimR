0b3e4a14bbac33aadf2dbd2d00ac43ed34ac3445
Carlo Pacioni <carlo.pacioni@gmail.com>
2018-08-02 16:13:44 +1000

    Edits and added decompress.

--- R/compress.logs.R
+++ R/compress.logs.R
@@ -1,4 +1,4 @@
-#' Compress log file and optinally remove them
+#' Compress log files and optinally remove them
 #'
 #' Compress log file and optinally remove them to save space and make
 #' simulations results more portable
@@ -15,7 +15,8 @@
 #' @param compression compression rate, integer between 0 and 9. See
 #'   \code{\link[base]{gzfile}}
 #' @inheritParams collate.census
-#' @return print to screen log files' full names and respective .gz
+#' @return a list where the first element is the list of log files found and the
+#'   second is the gz files created
 #' @importFrom R.utils gzip
 #' @export
 
@@ -26,21 +27,64 @@ compress.logs <- function(path.results=NULL, scenarios="all", delete.log=TRUE,
   #--------------------------------------------------------------------------#
 
 txt <- "Please, select the 'Results' folder within the workspace"
-if(is.null(path.results)) path.results <- tk_choose.dir(caption = txt)
+if(is.null(path.results)) path.results <- tk_choose.dir(caption=txt)
 suppressWarnings(if(scenarios == "all") 
   scenarios <- list.dirs(path=path.results, full.names=FALSE, recursive=FALSE))
 
-l.logs <- list.files(path = file.path(path.results, scenarios), pattern = ".log$", 
-                     full.names = TRUE, recursive = TRUE)
-gz.names <- sub(pattern = ".log$", replacement = ".gz", x =  l.logs) 
+l.logs <- list.files(path=file.path(path.results, scenarios), pattern=".log$", 
+                     full.names=TRUE, recursive=TRUE)
+gz.names <- sub(pattern=".log$", replacement=".gz", x= l.logs) 
 
-message("Starting compressing the log files")
+message("Starting compressing log files")
 t <- system.time(
   mapply(R.utils::gzip, l.logs,  destname=gz.names, 
        MoreArgs=list(compression=compression,  remove=delete.log, FUN=gzfile, 
                      ext="gz", overwrite=overwrite.gz))
 )
 message("Done!")
-message(paste("Elapsed time", t[3]))
+message(paste("Elapsed time in secs", t[3]))
+return(list(logs=l.logs, gzs=gz.names))
+}
+
+
+#' Decompress files with ".gz" extension, append ".log" extension and optinally
+#' remove the .gz files
+#'
+#' Decompress files with ".gz" extension, append ".log" extension and optinally
+#' remove the .gz files, which may be necessay if reports need to be created
+#'
+#' @param delete.gz whether the .gz file should be deleted after decompression
+#' @param overwrite.log if a .log file with the same name is present, should it be
+#'   overwritten?
+#' @inheritParams collate.census
+#' @return a list where the first element is the list of gz files found and the
+#'   second is the log files created
+#' @importFrom R.utils gzip
+#' @export
+
+decompress.gz <- function(path.results=NULL, scenarios="all", delete.gz=TRUE, 
+                          overwrite.log=TRUE) {
+  
+  
+  #--------------------------------------------------------------------------#
+  
+  txt <- "Please, select the 'Results' folder within the workspace"
+  if(is.null(path.results)) path.results <- tk_choose.dir(caption=txt)
+  suppressWarnings(if(scenarios == "all") 
+    scenarios <- list.dirs(path=path.results, full.names=FALSE, recursive=FALSE))
+  
+  l.gz <- list.files(path=file.path(path.results, scenarios), pattern=".gz$", 
+                       full.names=TRUE, recursive=TRUE)
+  log.names <- sub(pattern=".gz$", replacement=".log", x= l.gz) 
+  
+  message("Starting decompressing gz files")
+  t <- system.time(
+    mapply(R.utils::gunzip, l.gz,  destname=log.names, 
+           MoreArgs=list(remove=delete.gz, FUN=gzfile, 
+                         ext="gz", overwrite=overwrite.log))
+  )
+  message("Done!")
+  message(paste("Elapsed time in secs", t[3]))
+  return(list(logs=l.logs, gzs=gz.names))
 }
 
